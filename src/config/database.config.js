5c2b871e5fdd92b13655cf53be1a105260634cec
Rohit Watharkar <watharkarrohit7975@gmail.com>
2026-01-23 17:54:04 +0530

    render database url added

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -3,29 +3,29 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres",
-    logging: false,
+const isProduction = process.env.NODE_ENV === "production";
 
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-
-    define: {
-      freezeTableName: true,
-      underscored: true,
-      timestamps: true,
-    },
-  },
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        dialect: "postgres",
+        logging: false,
+      }
+    );
 
 export default sequelize;
