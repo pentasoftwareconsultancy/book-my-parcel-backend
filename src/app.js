38f513b40035e2be2c85d7c3a63a8afa45841bcb
Nexus CTC <154659272+pentasoftwareconsultancy@users.noreply.github.com>
2026-02-23 17:05:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 /* ✅ CORS configuration */
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://book-my-parcel-frontend.vercel.app"
+  "https://book-my-parcel-frontend.vercel.app/api"
 ];
 
 app.use(
