5494571563a0053a0a227178c519fd08a1647dda
Sahil7709 <sahiltheurkar3286@gmail.com>
2026-03-24 14:55:58 +0530

    waiting for merge pull

--- src/modules/traveller/traveller.dashboard.routes.js
+++ src/modules/traveller/traveller.dashboard.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { authMiddleware } from "../../middlewares/auth.middleware.js";
 import * as ctrl from "./traveller.controller.js";
 
-const router = express.Router();
+const router = express.Router(); 
 
 // Traveler dashboard endpoints (protected)
 router.get("/dashboard/deliveries", authMiddleware, ctrl.getTravelerDeliveries);
